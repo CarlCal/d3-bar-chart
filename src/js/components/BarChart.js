d95d08aee9e6ad4f37655041c6463cd58839be1e
Carl <callmer95@gmail.com>
2017-04-21 18:42:32 +0200

    more d3 tests

--- src/js/components/BarChart.js
+++ src/js/components/BarChart.js
@@ -5,29 +5,66 @@ import * as d3 from "d3"
 export default class BarChart extends React.Component {
 	render() {
 
-	var jsonRect = [
-    { "x_axis": 30, "y_axis": 30},
-    { "x_axis": 70, "y_axis": 70},
-    { "x_axis": 110, "y_axis": 100}];
 
- 
- var svgContainer = d3.select(".chart")
- 
- var rects = svgContainer.selectAll("rect")
-                           .data(jsonRect)
-                           .enter()
-                           .append("rect")
+var data = [
+  { "name": "Apples", "value": 4 },
+  { "name": "Pears", "value": 8 },
+  { "name": "Oranges", "value": 15},
+  { "name": "Mangos", "value": 16},
+  { "name": "Grapes", "value": 23},
+  { "name": "Bananas", "value": 42},
+]
 
- var rectAttributes = rects
-                       .attr("x", function (d) {return d.x_axis})
-                       .attr("y", function (d) {return d.y_axis})
-                       .attr("width", 10)
-                       .attr("height", 50)
-                       .style("fill", "black")
- 
+var margin = {top: 20, right: 30, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom
+
+var barWidth = width / data.length
+
+var x = d3.scaleBand()
+    .rangeRound([0, width], .1)
+    .domain(data.map(function(d) { return d.name }))
+
+var y = d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) { return d.value; })])
+    .range([height, 0])
+
+var xAxis = d3.axisBottom(x).tickFormat(function(d) { return d.value })
+var yAxis = d3.axisLeft(y).ticks(10 , "u")
+
+var chart = d3.select(".chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+chart.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis)
+
+chart.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+  .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Frequency");
+
+chart.selectAll(".bar")
+    .data(data)
+  .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) { return x(d.name) })
+    .attr("y", function(d) { return y(d.value) })
+    .attr("height", function(d) { return height - y(d.value)})
+    .attr("width", x.bandwidth() - 1)
+
 		return (
 			<main>
-				<svg class="chart" width="1000" height="500"></svg>
+				<svg class="chart"></svg>
 				<div class="tooltip"></div>
 			</main>
 		)
